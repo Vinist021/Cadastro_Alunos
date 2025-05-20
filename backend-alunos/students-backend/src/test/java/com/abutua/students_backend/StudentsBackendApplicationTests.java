9d5e2f9b0e15854a807b2ecb8e9a8c834ddee853
Vinicius Teixeira <vinist.002@gmail.com>
2025-05-20 15:40:28 -0300

    iniciada construção do backend

---
+++ backend-alunos/students-backend/src/test/java/com/abutua/students_backend/StudentsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abutua.students_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
