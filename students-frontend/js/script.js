5d7f075196653e49ba0ff8f16979c68d1feb214f
Vinicius Teixeira <vinist.002@gmail.com>
2025-05-25 08:38:04 -0300

    Bug de carregamento ao iniciar servidor consertado

--- students-frontend/js/script.js
+++ students-frontend/js/script.js
@@ -1,68 +1,65 @@
-//Máscaras
+//Masks
 $('#inputTelefone').mask('(00) 00000-0000');
 
-//Alunos já cadastrados
-var students = [
-    // {
-    //     id: 1,
-    //     name: 'Pedro Antônio',
-    //     email: 'pedro.antonio@abutua.com',
-    //     phone: '(15) 9999-9999',
-    //     idCurso: 1,
-    //     idPeriod: 3
-    // },
-    // {
-    //     id: 2,
-    //     name: 'Maria Francisca',
-    //     email: 'mariafr@gmail.com',
-    //     phone: '(15) 1234-5678',
-    //     idCurso: 2,
-    //     idPeriod: 2
-    // }
-];
+//Elements already registered
+let students = [];
 
-var courses = [
-    {id: 1, name: 'Java'},
-    {id: 2, name: 'Angular'},
-    {id: 3, name: 'SQL'}
-];
+let courses = [];
 
-var periods = [
-    {id: 1, name: 'Manhã'},
-    {id: 2, name: 'Tarde'},
-    {id: 3, name: 'Noite'}
-];
+let periods = [];
 
-
-
-//Carregar todos os students
-// function loadCourses() {
-//     $.ajax({
-//         url: 'http://localhost:8080/courses',
-//         type: 'GET',
-//         async: 'false',
-//         success: (response) => {
-//             courses = response;
-//             addCourses(courses);
-//         }
-//     })
-// }
-
-// function addCourses(courses) {
-//     for(let course of courses)
-//        document.getElementById('selectCurso').innerHTML += `<option value="${course.id}">${course.name}</option>`;
-// }
-
-function loadStudents() {
-    $.getJSON('http://localhost:8080/students', (response) => {
-        let students = response;
-        for(let stud of students)
-            addNewRow(stud);
+//Load functions
+function loadCourses() {
+    $.ajax({
+        url: 'http://localhost:8080/courses',
+        type: 'GET',
+        async: false,
+        success: (response) => {
+            courses = response;
+            addCourses(courses);
+        }
     })
 }
 
+function addCourses(courses) {
+    for(let course of courses)
+       document.getElementById('selectCurso').innerHTML += `<option value="${course.id}">${course.name}</option>`;
+}
+
+function loadPeriods() {
+    $.ajax({
+        url: 'http://localhost:8080/periods',
+        type: 'GET',
+        async: false,
+        success: (response) => {
+            periods = response;
+            addPeriods(periods);
+        }
+    })
+}
+
+function addPeriods() {
+    for(let period of periods)
+        document.getElementById('radioOptions').innerHTML += `
+     <div class="form-check">
+        <input class="form-check-input" type="radio" name="gridRadios" id="${period.id}" value="${period.id}" checked>
+        <label class="form-check-label" for="gridRadios1">${period.name}</label>
+    </div>
+    `
+}
+
+function loadStudents() {
+    $.getJSON('http://localhost:8080/students', (response) => {
+        students = response;
+        for(let student of students)
+            addNewRow(student);
+    })
+}
+
+//load all students
+loadCourses();
+loadPeriods();
 loadStudents();
-// loadCourses();
 
 function save() {
     const addStudent = 
@@ -72,7 +69,7 @@ function save() {
         email: document.getElementById('inputEmail').value,
         phone: document.getElementById('inputTelefone').value,
         idCurso: document.getElementById('selectCurso').value,
-        period: pegarTurno()
+        period: catchTurn()
     }
 
     addNewRow(addStudent);
@@ -81,45 +78,45 @@ function save() {
     document.getElementById('formAlunos').reset();
 }
 
-function pegarTurno() {
+function catchTurn() {
     const period = document.querySelector('input[name="gridRadios"]:checked').value;
     return period;
   }
 
 function addNewRow(student) {
-    var table = document.getElementById('studentsTable');
+    let table = document.getElementById('studentsTable');
 
-    var newRow = table.insertRow();
+    let newRow = table.insertRow();
     
-    //inserir id student
-    var idNode = document.createTextNode(student.id);
+    //insert student id 
+    let idNode = document.createTextNode(student.id);
     newRow.insertCell().appendChild(idNode);
 
-    //inserir name student
-    var nameNode = document.createTextNode(student.name);
+    //insert student name
+    let nameNode = document.createTextNode(student.name);
     newRow.insertCell().appendChild(nameNode);
 
-    //inserir email student
-    var emailNode = document.createTextNode(student.email);
-    var cellEmail = newRow.insertCell();
+    //insert student email
+    let emailNode = document.createTextNode(student.email);
+    let cellEmail = newRow.insertCell();
     cellEmail.className ='d-none d-md-table-cell';
     cellEmail.appendChild(emailNode);
 
-    //inserir phone student
-    var phoneNode = document.createTextNode(student.phone);
-    var cellPhone = newRow.insertCell();
+    //insert student phone
+    let phoneNode = document.createTextNode(student.phone);
+    let cellPhone = newRow.insertCell();
     cellPhone.className ='d-none d-sm-table-cell text-nowrap';
     cellPhone.appendChild(phoneNode);
 
-    //inserir curso student
-    var courseNode = document.createTextNode(courses[student.idCurso - 1].name);
-    var cellCourse = newRow.insertCell();
+    //insert student course
+    let courseNode = document.createTextNode(courses[student.idCurso - 1].name);
+    let cellCourse = newRow.insertCell();
     cellCourse.className = 'd-none d-sm-table-cell'
     cellCourse.appendChild(courseNode);
 
-    //inserir period student
-    var periodNode = document.createTextNode(periods[student.idPeriod - 1].name);
-    var cellShift = newRow.insertCell();
+    //insert student period
+    let periodNode = document.createTextNode(periods[student.idPeriod - 1].name);
+    let cellShift = newRow.insertCell();
     cellShift.className = 'd-none d-sm-table-cell'
     cellShift.appendChild(periodNode);
 }
